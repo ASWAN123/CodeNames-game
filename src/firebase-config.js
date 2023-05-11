60a6bec0b64c63df5a810c2351f20d0078482ee1
abderrahim assoaune <aaswan048@gmail.com>
2023-05-11 05:32:12 +0100

    setup gh-pages

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,7 +3,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 
-const firebaseConfig =process.env.REACT_APP_API_KEY
+const firebaseConfig =  {
+    apiKey: process.env.REACT_APP_API_KEY ,
+    authDomain: "hero3-f7cc1.firebaseapp.com" ,
+    projectId: "hero3-f7cc1" ,
+    storageBucket: "hero3-f7cc1.appspot.com" ,
+    messagingSenderId: "1097431111167" ,
+    appId: "1:1097431111167:web:b513a28a4a779ee22643ca"
+  }; 
 
 //do connection but not  sure
 firebase.initializeApp(firebaseConfig);
